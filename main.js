af217f12a4624f7a4da3024108813c3e5ed24d9a
Augie Bren <Augie>
2022-03-04 00:16:38 -0600

    done for the night

---
+++ main.js
@@ -0,0 +1,7 @@
+var today = new Date();
+var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+// display the date back to the index
+document.getElementsByClassName('date').textContent = `today ${dateTime}`
