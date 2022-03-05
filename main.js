85e0329c9b66b255598232142beca173760d85b2
AugieDog <>
2022-03-05 18:41:32 +0000

    work from replit

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ var today = new Date();
 var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
+console.log(dateTime)
 
-// display the date back to the index
-document.getElementsByClassName('date').textContent = `The Hacker Times ${dateTime}`
+
