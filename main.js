de46b9c4f513b75cbb730778e0ade25f5fd2d3f3
Augie Bren <Augie>
2022-03-06 10:03:42 -0600

    got the JS to work

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+// crate dateTime VAR
 var today = new Date();
 var date = today.getFullYear()+'/'+(today.getMonth()+1)+'/'+today.getDate();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 var dateTime = date+' '+time;
 console.log(dateTime)
 
-
+// display the dateTime Var
+document.getElementById('dateJS').textContent = `Today is ${dateTime}`;
